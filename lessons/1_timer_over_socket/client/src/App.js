2fa7020d18d75e7d947c26bc98fb9eb9330c73b7
Mike Blom <mikeblom@Mikes-MacBook-Pro.local>
2020-04-10 14:56:06 +0100

    first checkin

--- lessons/1_timer_over_socket/client/src/App.js
+++ lessons/1_timer_over_socket/client/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import DrawingForm from './DrawingForm';
+import DrawingList from './DrawingList';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Our awesome drawing app</h2>
         </div>
-        hello
+        <DrawingForm />
+        <DrawingList />
       </div>
     );
   }
---
+++ lessons/1_timer_over_socket/server/index.js
@@ -0,0 +1,46 @@
+const io = require('socket.io')();
+const r = require('rethinkdb');
+const port = 8000;
+
+function createDrawing({ connection, name }) {
+  r.table('drawings')
+    .insert({
+      name,
+      timestamp: new Date(),
+    })
+    .run(connection)
+    .then(() => {
+      console.log('created a drawing with name ', name);
+    });
+}
+
+function subscribeToDrawings({ client, connection }) {
+  r.table('drawings')
+    .changes({ include_initial: true })
+    .run(connection)
+    .then((cursor) => {
+      cursor.each((err, drawingRow) => {
+        client.emit('drawing', drawingRow.new_val);
+      });
+    });
+}
+
+r.connect({
+  host: 'localhost',
+  port: 28015,
+  db: 'awesome_whiteboard',
+}).then((connection) => {
+  io.on('connection', (client) => {
+    client.on('createDrawing', ({ name }) => {
+      createDrawing({ connection, name });
+    });
+
+    client.on('subscribeToDrawings', () =>
+      subscribeToDrawings({ client, connection })
+    );
+  });
+});
+
+io.listen(port);
+
+console.log('listening on port ', port);
